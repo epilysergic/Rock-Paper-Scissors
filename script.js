ae05980acb85e7e5bb7f0e40817ca8b1d0cfb5f0
Ata <epimethylate@gmail.com>
2022-05-09 17:59:32 -0400

    game complete, iteration works over 5 mathes

--- script.js
+++ script.js
@@ -1,64 +1,14 @@
-// IGNORE FIRST PART  FROM LINE 1 to LINE 49
-/* 
-// score of player vs computer variables
-let scorePlayer = 0
-let scoreComputer = 0
-let roundOutcome = ''
-// function to output a random choice between the three outcomes
-function computerPlay() {
-    let computerArray= ['Rock', 'Paper', 'Scissors']
-    let computerChoice = computerArray[Math.floor(Math.random()* computerArray.length)]
-    computerChoice = computerChoice.toUpperCase()
-    console.log(computerChoice)
-    return computerChoice
-}
-let computerPick = computerPlay().toUpperCase()
-// prompts player to select between Rock Paper Scissors.
-
-// round function to compare computer to player outputs and increase respective points
-function playRound(computerSelection) {
-    for (let round = 1 ; round< 5; round++) {
-        console.log(round)
-        const playerAlert = alert ('Player choose between the three options.\n Rock, Paper, Scissors')
-        let playerSelection = prompt ('Player chooose\n Rock, Paper, Scissors\n if no input then willl leave program')
-        playerSelection = playerSelection.toUpperCase()
-        console.log( 'player'. playerSelection)
-        switch (true) {
-            case(playerSelection == computerSelection):
-                roundOutcome = 'tie'
-                break;
-            case (playerSelection == 'PAPER' && computerSelection == 'ROCK' || 
-            playerSelection == 'SCISSORS' && computerSelection == 'PAPER'||
-            playerSelection == 'ROCK' && computerSelection == 'SCISSORS'):
-                roundOutcome = 'player wins'
-                scorePlayer += 1
-                break;
-            case (computerSelection == 'PAPER' && playerSelection == 'ROCK' || 
-                computerSelection == 'SCISSORS' && playerSelection == 'PAPER'|| 
-                computerSelection== 'ROCK' && playerSelection == 'SCISSORS'):
-                roundOutcome = 'computer wins'
-                scoreComputer += 1
-                break;
-            default:
-                alert ('Wrong input') 
-        }}}
-
-computerPlay()
-playRound(computerPick)
-*/
-
 // global score for both computer and player set to zero 
 computerScore = 0
 playerScore = 0
-// function to chose between rock paper scissors for computer
+// function to choose between rock paper scissors for computer
 function computerPlay(){                                            
     const computerArray = ['rock','paper','scissors']
-    let computerChoice = computerArray[Math.floor((Math.random() * computerArray.length))]
-    console.log(computerChoice)
-    return computerChoice
+    let computerSelection = computerArray[Math.floor((Math.random() * computerArray.length))] 
+    // console.log('computerChoice '+  computerChoice)
+    return computerSelection
 }
-// variable for player to choose option. 
-let playerChoice = prompt('What will player do ?? \n Either rock, paper or scissors (case insesitive)')
+
 //function to play single round of Rock Paper Scissors
 function gameRound( playerSelection , computerSelection) {
     playerSelect = playerSelection.toUpperCase()
@@ -66,6 +16,10 @@ function gameRound( playerSelection , computerSelection) {
     switch (true){
         // outcome when both players choose same option
         case (playerSelect == computerSelect):
+            console.log('computer score: ' + computerScore)
+            console.log('player score: ' + playerScore)
+            console.log('computer choice: ' + computerSelection)
+            console.log ('player choice: ' + playerSelect)
             console.log('TIE!!') 
             break;
         // outcome when player wins
@@ -73,7 +27,10 @@ function gameRound( playerSelection , computerSelection) {
         case (playerSelect == 'SCISSORS' && computerSelect == 'PAPER'):
         case (playerSelect == 'PAPER' && computerSelect == 'ROCK'):
             playerScore += 1 
-            console.log(playerScore)
+            console.log('computer score: ' + computerScore)
+            console.log('player score: ' + playerScore)
+            console.log('computer choice: ' + computerSelection)
+            console.log ('player choice: ' + playerSelect)
             console.log ('Player wins !! ' + playerSelect + ' Beats ' + computerSelect)
             return playerScore
             break;
@@ -82,27 +39,50 @@ function gameRound( playerSelection , computerSelection) {
         case (playerSelect == 'PAPER' && computerSelect == 'SCISSORS'):
         case (playerSelect == 'SCISSORS' && computerSelect == 'ROCK'):
             computerScore += 1
-            console.log(computerScore)
+            console.log('computer score: ' + computerScore)
+            console.log('player score: ' + playerScore)
+            console.log('computer choice: ' + computerSelection)
+            console.log ('player choice: ' + playerSelect)
             console.log ('Computer wins!! ' + computerSelect + ' Beats ' + playerSelect)
             return computerScore
             break;
-        //outcome when player chooses wrong option
+        //outcome when player writes wrong input
         default:
             console.log('please choose correct output retry')
             break;
 }}
-// game out of five where it stops at either player/bot reaching score of 5 and compares to determine winner 
-function game(round){
+// Once a point of 5 reached from either side it will compare and announce who is the winner  
+function gameWinner(round){
+    // compare points once one reaches a point of 5 
     if (computerScore == 5 || playerScore == 5 ) {
+        // if computer wins output
         if (computerScore > playerScore) {
-            console.log ('Computer is the winner: ' + computerScore +' versus players petite score of ' + playerScore)
+            console.log ('Computer is the winner: ' + computerScore +' versus players small score of ' + playerScore)
         }
+        // if player wins output
         else if ( playerScore > computerScore){
-            console.log ('Player is the winner: ' + playerScore +' versus the bots petite score of ' + computerScore)
+            console.log ('Player is the winner: ' + playerScore +' versus the bots small score of ' + computerScore)
         }
+        // will never happen just for fun
         else if ( playerScore == computerScore){
             console.log ('NO WINNER!! THIS MATCH WAS A SCAM!!')
         }
     }
 }
-game(gameRound(playerChoice, computerPlay()))
+// 
+function gameTotal(){
+ 
+    while ((playerScore <= 5) || (computerScore <= 5)) {
+        // variable for player to choose option. 
+        let playerChoice = prompt('What will player do ?? \n Either rock, paper or scissors (case insesitive)')
+        // gameWinner calls to each round and compares winner at the end
+        gameWinner(gameRound(playerChoice,computerPlay()))
+        // if score reaches 5 on either side the prompt will be closed
+        if ((playerScore == 5 || computerScore == 5 )){
+            console.log('Thanks for playing the game') 
+            break;  
+        }
+  
+}}
+gameTotal()
+
